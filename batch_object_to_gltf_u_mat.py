87fcfea1d02a50f4837686a9733f27745f18f859
Kevin Loustau <kevin.loustau@gmail.com>
2023-02-17 11:02:48 -0300

    array of func classes, also move to sidepanel

--- batch_object_to_gltf_u_mat.py
+++ batch_object_to_gltf_u_mat.py
@@ -1,11 +1,13 @@
+import os
+import bpy
 bl_info = {
-    "name": "Unique Mat to Object",
-    "author": "Kevin Loustau",
-    "blender": (3, 4, 0)
+    "name": "Super Mostache",
+    "blender": (3, 4, 0),
+    'version': (0, 0, 1),
+    'author': 'Kevin Loustau',
+    'description': 'Super Mostache',
 }
 
-import bpy
-import os
 
 def remove_data_materials():
     for material in bpy.data.materials:
@@ -13,12 +15,15 @@ def remove_data_materials():
         bpy.data.materials.remove(material)
 
 # func create all materials
+
+
 def assign_unique_mat():
     scene = bpy.context.scene
     for obj in scene.objects:
         material = bpy.data.materials.new(name=obj.name)
         obj.data.materials.clear()
-        obj.data.materials.append(material)    
+        obj.data.materials.append(material)
+
 
 def exports_objects_to_gltf():
     # export to blend file location
@@ -38,7 +43,7 @@ def exports_objects_to_gltf():
 
         obj.select_set(True)
         previous_location = obj.location.copy()
-        obj.location = [0,0,0]
+        obj.location = [0, 0, 0]
         view_layer.objects.active = obj
         name = bpy.path.clean_name(obj.name)
         fn = os.path.join(basedir, name)
@@ -46,7 +51,7 @@ def exports_objects_to_gltf():
         obj.select_set(False)
         print("written:", fn)
         obj.location = previous_location
-        
+
     view_layer.objects.active = obj_active
 
     for obj in selection:
@@ -54,29 +59,46 @@ def exports_objects_to_gltf():
 
 
 class BatchObjectToGLTFWithUniqueMat(bpy.types.Operator):
-    """Batch Object-To-GLTF- with unique material"""  
+    """Batch Object-To-GLTF- with unique material"""
     bl_idname = "object.batch_objects"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Objects -> Unique Mat -> Export GLTF"         # Display name in the interface.
-    
-    def execute(self, context): 
+    # Display name in the interface.
+    bl_label = "Objects -> Unique Mat -> Export GLTF"
+
+    def execute(self, context):
         remove_data_materials()
         assign_unique_mat()
         exports_objects_to_gltf()
         return {'FINISHED'}
-    
-def menu_func(self, context):
-    self.layout.operator(BatchObjectToGLTFWithUniqueMat.bl_idname)
+
+
+class SP_TOOL_PANEL(bpy.types.Panel):
+    bl_idname = 'VIEW3D_PT_SUPER_MOSTACHE'
+    bl_label = 'Super Mostache'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Super Mostache'
+
+    def draw(self, context):
+        self.layout.label(text='Object > U. Mat > Gltf')
+        self.layout.operator(
+            BatchObjectToGLTFWithUniqueMat.bl_idname, text='Unique Mat + Export')
+
+
+CLASSES = [
+    BatchObjectToGLTFWithUniqueMat,
+    SP_TOOL_PANEL,
+]
+
 
 def register():
-    bpy.utils.register_class(BatchObjectToGLTFWithUniqueMat)
-    bpy.types.VIEW3D_MT_object.append(menu_func)  # Adds the new operator to an existing menu.
+    for klass in CLASSES:
+        bpy.utils.register_class(klass)
+
 
 def unregister():
-    bpy.utils.unregister_class(ObjectMoveX)
-
+    for klass in CLASSES:
+        bpy.utils.unregister_class(klass)
 
 
 if __name__ == "__main__":
     register()
-
-
